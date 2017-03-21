8a64a4ce059eaae3d0066c63ecffc12801c30355
nadia <nadiagnieto@gmail.com>
2017-03-21 13:45:27 +0100

    gpgga parser without round

--- practica1/src/utils/GpggaParser.java
+++ practica1/src/utils/GpggaParser.java
@@ -1,7 +1,5 @@
 package utils;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 public class GpggaParser {
 
@@ -37,48 +35,40 @@ public class GpggaParser {
 	 */
 	public static void setup(double degreeLatitude, double minLatitude, double degreeLongitude, double minLongitude, boolean isWestLongitude) {
 		ro = convertDegreesToRadians(degreeLatitude, minLatitude);
-		ro = round(ro, 5);
 		if (isWestLongitude) {
 			lambda = -(convertDegreesToRadians(degreeLongitude, minLongitude));
 		}
 		else{
 			lambda = convertDegreesToRadians(degreeLongitude, minLongitude);
-		}
-		lambda = round(lambda, 5);
+		};
 	}
 
 	private static double getLambdaSub0() {
-		System.out.println("LAMBDA0 "+round(centerLambda * (Math.PI / 180),9));
-		return round(centerLambda * (Math.PI / 180),9);
+		return centerLambda * (Math.PI / 180);
 	}
 
 	private static double getESquarePrime() {
-		System.out.println("EsquareP "+round(eSquare / (1 - eSquare),10));
-		return round(eSquare / (1 - eSquare),10);
+		return eSquare / (1 - eSquare);
 	}
 
 	private static double N(){
 		double sinRo = Math.sin(ro);
 		double square = Math.sqrt( 1 - (eSquare* Math.pow(sinRo, 2.0)));
-		System.out.println("N "+round(a / square, 6));
-		return round(a / square, 6);
+		return a / square;
 	}
 
 	private static double T(){
 		double tan = Math.tan(ro);
-		System.out.println("T "+round(Math.pow(tan, 2.0), 10));
-		return round(Math.pow(tan, 2.0), 10);
+		return Math.pow(tan, 2.0);
 	}
 	
 	private static double C(){
 		double cosRo = Math.cos(ro);
-		System.out.println("C "+round(getESquarePrime() * Math.pow(cosRo, 2.0), 8));
-		return round(getESquarePrime() * Math.pow(cosRo, 2.0), 8);
+		return getESquarePrime() * Math.pow(cosRo, 2.0);
 	}
 	
 	private static double A(){
-		System.out.println("A "+round(Math.cos(ro) * (lambda - getLambdaSub0()), 10));
-		return round(Math.cos(ro) * (lambda - getLambdaSub0()), 10);
+		return Math.cos(ro) * (lambda - getLambdaSub0());
 	}
 	
 	private static double M(){
@@ -86,21 +76,18 @@ public class GpggaParser {
 		double second = ( ( 3/8 * eSquare ) + ( 3/32 * (Math.pow(eSquare, 2.0)) ) + ( 45/1024 * (Math.pow(eSquare, 3.0)) ) ) * Math.sin(2*ro);
 		double third = ( ( 15/256 * (Math.pow(eSquare, 2.0)) ) + ( 45/1024 * (Math.pow(eSquare, 3.0)) ) ) * Math.sin(4*ro);
 		double fourth =  ( 35/3072 * (Math.pow(eSquare, 3.0))) * Math.sin(6*ro);
-		System.out.println("M "+round(a * ( first - second + third - fourth ), 4));
-		return round(a * ( first - second + third - fourth ), 4);
+		return a * ( first - second + third - fourth );
 	}
 
 	private static double convertDegreesToRadians(double degrees, double minutes) {
 		double angle = degrees + (minutes / 60.0);
-//		System.out.println("GRADOS "+angle);
-//		System.out.println("RADIANS "+Math.toRadians(angle));
 		return Math.toRadians(angle);
 	}
 	
 	/**Return the UMT easting in radians*/
 	public static double getUMTEasting(){
 		double second = ( A() + (((1 - T() + C()) * Math.pow(A(), 3.0))/6 + (( 5 - (18*T()) + Math.pow(T(), 2.0) + (72 * C()) - (58 * eSquare) ) * Math.pow(A(), 3.0))/120) );
-		return round((0.9996 * N() * second) + 500000, 6);
+		return (0.9996 * N() * second) + 500000;
 	}
 	
 	/**Return the UMT easting in radians*/
@@ -108,14 +95,6 @@ public class GpggaParser {
 		double first = (M() + N()) * Math.tan(ro);
 		double second = ( Math.pow(A(), 2.0)/2 + ( (5 - T() + (9 * C()) + (4 * Math.pow(C(), 2.0)) ) * Math.pow(A(), 4.0))/24 );
 		double third = ( (( 61 - (58 * T()) + Math.pow(T(), 2.0) + (600 * C()) - (330 * eSquare) ) * Math.pow(A(), 6.0))/720 );
-		return round(0.9996 * ( first * ( second + third )), 6);
-	}
-	
-	
-	public static double round(double value, int places) {
-	    if (places < 0) throw new IllegalArgumentException();
-	    BigDecimal bd = new BigDecimal(value);
-	    bd = bd.setScale(places, RoundingMode.DOWN);
-	    return bd.doubleValue();
+		return 0.9996 * ( first * ( second + third ));
 	}
 }
