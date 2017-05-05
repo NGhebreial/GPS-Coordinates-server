96c6b6196e5fe8ee0c4ab080fd8a0d57a8058bf6
nadia <nadiagnieto@gmail.com>
2017-05-05 19:49:50 +0200

    DataPoint model without UTMConverter

--- practica1/src/models/DataPoint.java
+++ practica1/src/models/DataPoint.java
@@ -1,11 +1,11 @@
 package models;
 
-import math.UTMConverter;
 import utils.Coordinate;
 
 public class DataPoint {
 	
-	private UTMConverter utm;
+	private Double norting;
+	private Double easting;
 	private Double speed;
 	private Coordinate coordinate;
 	private Double bearing;
@@ -14,20 +14,29 @@ public class DataPoint {
 	public DataPoint(){		
 	}
 	
-	public DataPoint(UTMConverter utm, Double speed, Coordinate coordinate, Double bearing, Long miliseconds){
-		this.utm = utm;
+	public DataPoint(Double norting, Double easting, Double speed, Coordinate coordinate, Double bearing, Long miliseconds){
+		this.norting = norting;
+		this.easting = easting;
 		this.speed = speed;
 		this.coordinate = coordinate;
 		this.bearing = bearing;
 		this.miliseconds = miliseconds;
 	}
-
-	public UTMConverter getUtm() {
-		return utm;
+	
+	public Double getNorting() {
+		return norting;
 	}
 
-	public void setUtm(UTMConverter utm) {
-		this.utm = utm;
+	public void setNorting(Double norting) {
+		this.norting = norting;
+	}
+
+	public Double getEasting() {
+		return easting;
+	}
+
+	public void setEasting(Double easting) {
+		this.easting = easting;
 	}
 
 	public Double getSpeed() {
@@ -64,8 +73,8 @@ public class DataPoint {
 
 	@Override
 	public String toString() {
-		return "DataPoint [utm=" + utm + ", speed=" + speed + ", coordinate=" + coordinate + ", bearing=" + bearing
-				+ ", miliseconds=" + miliseconds + "]";
+		return "DataPoint [norting=" + norting + ", easting=" + easting + ", speed=" + speed + ", coordinate="
+				+ coordinate + ", bearing=" + bearing + ", miliseconds=" + miliseconds + "]";
 	}	
 	
 }
