42e768ae27d2b828b6181eedfd35098987c2c2e6
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2017-05-24 13:40:04 +0200

    Address and ide modules

--- src/main/java/entry/main.java
+++ src/main/java/entry/main.java
@@ -5,7 +5,7 @@ import utils.Controller;
 public class main {
 
     public static void main( String[] args ){
-        Controller c = new Controller("192.168.43.1", 9090, 8978);
+        Controller c = new Controller("192.168.1.54", 9090, 8978);
         c.start();
     }
 }
